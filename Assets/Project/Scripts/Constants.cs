0ab5cbcb0613291fc858be4140039199639609a2
roliauo <roliauo@gmail.com>
2021-03-12 01:20:00 +0800

    [Build] Add Scenes (Title/ Game/ Win/ Lose) and MenuButton script

---
+++ Assets/Project/Scripts/Constants.cs
@@ -0,0 +1,23 @@
+﻿public class Constants 
+{
+    // From InputManager (Edit > ProjectSettings > InputManager)
+    public const string VERTICAL = "Vertical";
+    public const string HORIZONTAL = "Horizontal";
+    public const string MOUSE_Y = "Mouse Y";
+    public const string MOUSE_X = "Mouse X";
+    public const string JOYSTICK_LOOK_Y = "Look Y";
+    public const string JOYSTICK_LOOK_x = "Look X";
+    public const string BUTTON_JUMP = "Jump";
+    public const string BUTTON_FIRE = "Fire";
+    public const string BUTTON_GAMEPAD_FIRE = "Gamepad Fire";
+    public const string BUTTON_SPRINT = "Sprint";
+    public const string BUTTON_CROUCH = "Crouch";
+    public const string BUTTON_AIM = "Aim";
+    public const string BUTTON_GAMEPAD_AIM = "Gamepad Aim";
+    public const string BUTTON_SWITCH_WEAPON = "Mouse ScrollWheel";
+    public const string BUTTON_GAMEPAD_SWITCH_WEAPON = "Gamepad Switch";
+    public const string BUTTON_NEXT_WEAPON = "NextWeapon";
+    public const string BUTTON_PAUSE_MENU = "Pause Menu";
+    public const string BUTTON_SUBMIT = "Submit";
+    public const string BUTTON_CANCEL = "Cancel";
+}
---
+++ Assets/Project/Scripts/UI/MenuButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+    public string sceneToLoad = "";
+
+    private void Update()
+    {
+        
+    }
+
+    public void LoadTargetScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void ShowOptionsMenu()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+
+}
