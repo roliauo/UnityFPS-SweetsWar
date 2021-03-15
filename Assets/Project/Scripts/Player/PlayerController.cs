db270dc39779e02ddcc3972e0cdd482a4672da30
roliauo <roliauo@gmail.com>
2021-03-15 23:08:27 +0800

    Player control - sprint, crouch, jump, fire

--- Assets/Project/Scripts/Player/PlayerController.cs
+++ Assets/Project/Scripts/Player/PlayerController.cs
@@ -10,33 +10,90 @@ public class PlayerController : MonoBehaviour
     public AudioSource playerAudioSource;
 
     public float lookSensitivity = 200f;
-    public float gravity = 20f;
+    public float gravity = -20f; 
+
+    [Header("Ground Check")]
+    public Transform groundCheckTransform;
+    public LayerMask groundLayerMask;
+    public float groundDistance = 0.4f;
+
 
     [Header("Movement")]
-    public float speed = 4f;
-    public float maxSpeed = 10f;
-    public float speedCrouching = 3f;
-    public float jumpForce = 9f;
+    public float speedNormal = 4f;
+    public float speedMax = 8f;
+    public float speedCrouching = 2.5f;
+    public float speedSprinting = 6f;
+    public float jumpHeight = 1f;
 
     [Header("Stance")]
-    public float heightStanding = 2f;
+    public float heightStanding = 1.5f;
     public float heightCrouching = 1f;
+
+    public bool isDead { get; private set; }
+    public bool isGrounded { get; private set; }
+    public bool isCrouching { get; private set; }
 
 
     // private
     CharacterController m_characterController;
+    float m_cameraHeightRatio = 0.9f;
     float m_rotationX = 0f;
     float m_heightPlayer;
+    float m_speedPlayer;
+    Vector3 m_velocity;
+    
 
 
     void Start()
     {
         m_characterController = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
+        m_speedPlayer = speedNormal;
+        m_heightPlayer = heightStanding;
     }
 
     void Update()
     {
+        checkGrounded();
+
+        // sprint
+        m_speedPlayer = isGrounded && Input.GetButton(GameConstants.BUTTON_SPRINT) ? speedSprinting : speedNormal;
+        
+        // crouch
+        if (isGrounded && Input.GetButtonDown(GameConstants.BUTTON_CROUCH))
+        {
+            isCrouching = !isCrouching;
+            m_heightPlayer = isCrouching ? heightCrouching : heightStanding;
+            m_speedPlayer = isCrouching ? speedCrouching : speedNormal;
+            
+            // resize player
+            transform.localScale = new Vector3(1, m_heightPlayer, 1);
+
+            /*if (m_characterController.height != m_heightPlayer)
+            {
+                // resize player and adjust camera position
+                m_characterController.height = m_heightPlayer;
+                m_characterController.center = isCrouching ? new Vector3(0, 0.5f, 0) : new Vector3(0, 0, 0);
+                //groundCheckTransform.localPosition = 
+                //Vector3.up * m_heightPlayer * 0.5f;
+                Debug.Log("m_characterController.center: " + m_characterController.center);
+                playerCamera.transform.localPosition = Vector3.Lerp(playerCamera.transform.localPosition, Vector3.up * m_heightPlayer * m_cameraHeightRatio, 10 * Time.deltaTime);
+                //m_Actor.aimPoint.transform.localPosition = m_characterController.center;
+            }*/
+        }
+
+        // jump
+        if (isGrounded && Input.GetButtonDown(GameConstants.BUTTON_JUMP))
+        {
+            m_velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // fire
+        if (Input.GetButtonDown(GameConstants.BUTTON_FIRE))
+        {
+            Debug.Log("BUTTON_FIRE");
+        }
+
         /* look */
         float mouseX = Input.GetAxis(GameConstants.MOUSE_X) * lookSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis(GameConstants.MOUSE_Y) * lookSensitivity * Time.deltaTime;
@@ -47,8 +104,22 @@ public class PlayerController : MonoBehaviour
         transform.Rotate(Vector3.up * mouseX);
 
         /* move */
-        Vector3 move = transform.right * Input.GetAxis(GameConstants.HORIZONTAL) +
-                       transform.forward * Input.GetAxis(GameConstants.VERTICAL);
-        m_characterController.Move(move * speed * Time.deltaTime);
+        float x = Input.GetAxis(GameConstants.HORIZONTAL);
+        float y = Input.GetAxis(GameConstants.VERTICAL);
+        Vector3 move = transform.right * x + transform.forward * y;
+        m_characterController.Move(move * m_speedPlayer * Time.deltaTime);  
+
     }
+
+    void checkGrounded()
+    {
+        isGrounded = Physics.CheckSphere(groundCheckTransform.position, groundDistance, groundLayerMask);
+        if (isGrounded && m_velocity.y < 0)
+        {
+            m_velocity.y = -2f;
+        }
+        m_velocity.y += gravity * Time.deltaTime;
+        m_characterController.Move(m_velocity * Time.deltaTime);
+    }
+
 }
