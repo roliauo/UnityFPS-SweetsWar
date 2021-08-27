7850ebd69d448a4ea4e90cf2554c6723faed56cd
roliauo <roliauo@gmail.com>
2021-08-27 16:56:13 +0800

    [Item] Add Item and Inventory

---
+++ Assets/Project/Scripts/Inventory.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.SweetsWar
+{
+    [CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory/New Inventory")]
+    public class Inventory : ScriptableObject
+    {
+        public List<Item> ItemList = new List<Item>();
+    }
+}
---
+++ Assets/Project/Scripts/Item.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.SweetsWar
+{
+    [CreateAssetMenu(fileName = "New Item", menuName = "Inventory/New Item")]
+    public class Item: ScriptableObject
+    {
+        public int ID;
+        public string DisplayName;
+        public Sprite Image;
+        public int Number;
+
+        [TextArea]
+        public string Description;
+    }
+}
+
