36923a3ac3083f4045d71e7dfafbda2f2fcdf257
roliauo <roliauo@gmail.com>
2021-09-24 16:57:43 +0800

    [Item] modified items and add item prefabs

---
+++ Assets/Project/Scripts/UI/BackpackSlotPrefab.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.SweetsWar
+{
+    public class BackpackSlotPrefab : MonoBehaviour
+    {
+        public Button btn_Slot;
+
+        private Item m_item;
+        void Start()
+        {
+            btn_Slot.onClick.AddListener(() =>
+            {
+                Debug.Log("slot click: " + m_item.DisplayName);
+            });
+        }
+
+        public void SetItem(Item item)
+        {
+            m_item = item;
+            btn_Slot.image.sprite = item.Image;            
+            btn_Slot.GetComponentInChildren<Text>().text = item.DisplayName;
+            Debug.Log("DisplayName: " + item.DisplayName);
+        }
+    }
+
+}
