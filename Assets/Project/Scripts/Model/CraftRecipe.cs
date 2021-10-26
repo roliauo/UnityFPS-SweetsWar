7ef84a269a37056e89b9f7bf3e1b11f647c15eec
roliauo <roliauo@gmail.com>
2021-10-26 15:33:01 +0800

    [Craft] Add Data Model: Recipe

---
+++ Assets/Project/Scripts/Model/CraftRecipe.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.SweetsWar
+{
+    [CreateAssetMenu(fileName = "New Craft Recipe", menuName = "Craft/New Recipe")]
+    public class CraftRecipe : ScriptableObject
+    {
+        public short OutputItemID;
+        public Ingredient[] Ingredients;
+       
+        /*
+        public CraftRecipe(short outputItemID, Ingredient[] ingredients)
+        {
+            this.Ingredients = ingredients;
+            this.OutputItemID = outputItemID;
+        }
+        */
+    }
+
+}
