dd8c196b6f089c5508da771779e0072006117204
roliauo <roliauo@gmail.com>
2021-11-30 13:48:40 +0800

    [Weapon] Bullet

---
+++ Assets/Project/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.SweetsWar
+{
+    public class Bullet : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            Destroy(gameObject);
+
+            
+            if (other.tag == GameConstants.TAG_PLAYER) {
+                Debug.Log("hit Player!");
+            }
+            
+        }
+    }
+}
