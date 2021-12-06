1b8f4127da0eed230f4bc1709173f50bf7195eb9
roliauo <roliauo@gmail.com>
2021-12-06 23:02:15 +0800

    add DontDestoryOnLoad.cs

---
+++ Assets/Project/Scripts/DontDestoryOnLoad.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.SweetsWar
+{
+    public class DontDestoryOnLoad : MonoBehaviour
+    {        
+        private void Start()
+        {
+            GameObject[] objs = GameObject.FindGameObjectsWithTag(tag);
+
+            if (objs.Length > 1)
+            {
+                Destroy(this.gameObject);
+            }
+
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+}
